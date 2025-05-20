0af4df9dd45a4cb2763e9de6ee0bede1c3ca7303
AmandaYang679 <mikheevaanna08@gmail.com>
2025-05-21 00:02:06 +0300

    почищен код от комментариев

--- core/library/views.py
+++ core/library/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Library, Book
 
-# Create your views here.
-# def book_list(request, library_slug=None):
-#     libraries = Library.objects.all()
-#     books = Book.objects.filter(available=True)
-    
-#     library = None
-#     if library_slug:
-#         library = get_object_or_404(Library, slug=library_slug)
-#         books = Book.objects.filter(library=library) # первое library - это из models, где ForeignKey, а второе library - это 11 строка
-    
-#     return render(request, 'library/library.html', 
-#                   {'library': library,
-#                    'libraries': libraries,
-#                    'books': books})
-
 
 def homepage(request):
     recommended_books = Book.objects.all()[:10]
@@ -29,7 +14,7 @@ def libraries_list(request):
 
 def books_list_in_library(request, slug):
     library = get_object_or_404(Library, slug=slug)
-    books = Book.objects.filter(library=library)
+    books = Book.objects.filter(library=library) # первое library - это из models, где ForeignKey, а второе library - это 11 строка
     return render(request, 'library/books_in_library.html', {'books': books, 'library': library})
     
     
