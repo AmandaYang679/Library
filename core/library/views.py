676b3bc92d811f09857b7b1ac35bd4a43d104fba
AmandaYang679 <mikheevaanna08@gmail.com>
2025-05-21 21:28:46 +0300

    update template's structure and fix in homepage template

--- core/library/views.py
+++ core/library/views.py
@@ -4,7 +4,7 @@ from .models import Library, Book
 
 def homepage(request):
     recommended_books = Book.objects.all()[:10]
-    return render(request, 'library/homepage.html', {'r_books': recommended_books})
+    return render(request, 'homepage.html', {'r_books': recommended_books})
 
 
 def libraries_list(request):
@@ -15,15 +15,15 @@ def libraries_list(request):
 def books_list_in_library(request, slug):
     library = get_object_or_404(Library, slug=slug)
     books = Book.objects.filter(library=library) # первое library - это из models, где ForeignKey, а второе library - это 11 строка
-    return render(request, 'library/books_in_library.html', {'books': books, 'library': library})
+    return render(request, 'books/books_in_library.html', {'books': books, 'library': library})
     
     
 def book_detail(request, slug):
     book = get_object_or_404(Book, slug=slug, available=True)
-    return render(request, 'library/book_detail.html', {'book': book})
+    return render(request, 'books/book_detail.html', {'book': book})
 
 
 def all_books_list(request):
     books = Book.objects.all()
-    return render(request, 'library/all_books_list.html', {'books': books})
+    return render(request, 'books/all_books_list.html', {'books': books})
     
